2d1836cdb4fd5d9005b19bb48daa78b393b08478
Ilya <iosdsbb@gmail.com>
2025-10-28 12:30:14 +0300

    feat: added btn logic

--- src/app.tsx
+++ src/app.tsx
@@ -3,72 +3,104 @@ import styles from '~/app.module.css';
 import data from '~/data.json';
 
 export const App = () => {
-	const [steps, _] = useState<typeof data>(data);
-	const [activeIndex, setActiveIndex] = useState(0);
+    const [steps, _] = useState<typeof data>(data);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-	const [isFirst, setIsFirst] = useState(true);
-	const [isLast, setIsLast] = useState(false);
+    const [isFirst, setIsFirst] = useState(true);
+    const [isLast, setIsLast] = useState(false);
 
-	const nextStep = () => {
-		setActiveIndex((index) => index + 1);
-	};
+    const nextStep = () => {
+        setActiveIndex((index) => {
+            const newIndex = index + 1;
+            if (newIndex === 0) {
+                setIsFirst(true);
+                setIsLast(false);
 
-	const prevStep = () => {
-		setActiveIndex((index) => index - 1);
-	};
+            } else if (newIndex === steps.length - 1) {
+                setIsFirst(false);
+                setIsLast(true);
 
-	const startStep = () => {
+            } else {
+                setIsFirst(false);
+                setIsLast(false);
+            }
+            return newIndex;
+        });
+    };
 
-	};
+    const prevStep = () => {
+        setActiveIndex((index) => {
+            const newIndex = index - 1;
+            if (newIndex === 0) {
+                setIsFirst(true);
+                setIsLast(false);
 
-	const selectStep = (event) => {
-		const id = Number(event.target.dataset.id);
-		setActiveIndex(id);
+            } else if (newIndex === steps.length - 1) {
+                setIsFirst(false);
+                setIsLast(true);
 
-		if (id === 0) {
-			setIsFirst(true);
-			setIsLast(false);
+            } else {
+                setIsFirst(false);
+                setIsLast(false);
+            }
+            return newIndex;
+        });
+    };
 
-		} else if (id === steps.length - 1) {
-			setIsFirst(false);
-			setIsLast(true);
+    const startStep = () => {
+        setActiveIndex(0);
+        setIsFirst(true);
+        setIsLast(false);
+    };
 
-		} else {
-			setIsFirst(false);
-			setIsLast(false);
-		}
-	};
+    const selectStep = (event) => {
+        const id = Number(event.target.dataset.id);
+        setActiveIndex(id);
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.card}>
-				<h1>Инструкция по готовке пельменей</h1>
-				<div className={styles.steps}>
-					<div className={styles['steps-content']}>
-						{steps[activeIndex].content}
-					</div>
-					<ul className={styles['steps-list']}>
-						{steps.map((step, i) =>
-							(<li key={step.id} className={
-								styles['steps-item'] +
-								" " + (activeIndex - i >= 0 ? styles.done : "") +
-								" " + (i === activeIndex ? styles.active : "")
-							}>
-								<button onClick={selectStep} data-id={i} className={styles['steps-item-button']}>
-									{i + 1}
-								</button>
-								Шаг {i + 1}
-							</li>)
-						)}
-					</ul>
-					<div className={styles['buttons-container']}>
-						<button className={styles.button} onClick={prevStep} disabled={isFirst}>Назад</button>
-						<button className={styles.button} onClick={nextStep} disabled={isLast}>
-							Далее
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        if (id === 0) {
+            setIsFirst(true);
+            setIsLast(false);
+
+        } else if (id === steps.length - 1) {
+            setIsFirst(false);
+            setIsLast(true);
+
+        } else {
+            setIsFirst(false);
+            setIsLast(false);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <h1>Инструкция по готовке пельменей</h1>
+                <div className={styles.steps}>
+                    <div className={styles['steps-content']}>
+                        {steps[activeIndex].content}
+                    </div>
+                    <ul className={styles['steps-list']}>
+                        {steps.map((step, i) =>
+                            (<li key={step.id} className={
+                                styles['steps-item'] +
+                                " " + (activeIndex - i >= 0 ? styles.done : "") +
+                                " " + (i === activeIndex ? styles.active : "")
+                            }>
+                                <button onClick={selectStep} data-id={i} className={styles['steps-item-button']}>
+                                    {i + 1}
+                                </button>
+                                Шаг {i + 1}
+                            </li>)
+                        )}
+                    </ul>
+                    <div className={styles['buttons-container']}>
+                        <button className={styles.button} onClick={prevStep} disabled={isFirst}>Назад</button>
+                        <button className={styles.button} onClick={isLast ? startStep : nextStep}>
+                            {isLast ? 'Начать сначала' : 'Далее'}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
