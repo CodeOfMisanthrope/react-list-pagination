106e14516cfa1ad373b3496d845491618b51e805
Ilya <iosdsbb@gmail.com>
2025-10-28 11:44:50 +0300

    init

---
+++ src/app.tsx
@@ -0,0 +1,74 @@
+import {useState} from "react";
+import styles from '~/app.module.css';
+import data from '~/data.json';
+
+export const App = () => {
+	const [steps, _] = useState<typeof data>(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const [isFirst, setIsFirst] = useState(true);
+	const [isLast, setIsLast] = useState(false);
+
+	const nextStep = () => {
+		setActiveIndex((index) => index + 1);
+	};
+
+	const prevStep = () => {
+		setActiveIndex((index) => index - 1);
+	};
+
+	const startStep = () => {
+
+	};
+
+	const selectStep = (event) => {
+		const id = Number(event.target.dataset.id);
+		setActiveIndex(id);
+
+		if (id === 0) {
+			setIsFirst(true);
+			setIsLast(false);
+
+		} else if (id === steps.length - 1) {
+			setIsFirst(false);
+			setIsLast(true);
+
+		} else {
+			setIsFirst(false);
+			setIsLast(false);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((step, i) =>
+							(<li key={step.id} className={
+								styles['steps-item'] +
+								" " + (activeIndex - i >= 0 ? styles.done : "") +
+								" " + (i === activeIndex ? styles.active : "")
+							}>
+								<button onClick={selectStep} data-id={i} className={styles['steps-item-button']}>
+									{i + 1}
+								</button>
+								Шаг {i + 1}
+							</li>)
+						)}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button} onClick={prevStep} disabled={isFirst}>Назад</button>
+						<button className={styles.button} onClick={nextStep} disabled={isLast}>
+							Далее
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import * as path from "node:path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+const dirname = import.meta.dirname;
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '~': path.resolve(dirname, 'src'),
+      '~assets': path.resolve(dirname, 'assets'),
+    }
+  }
+})
